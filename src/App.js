52f02474c57b3653e9e58a1920f1bbb357998fc9
Julio Perez <63935692+jc-prz@users.noreply.github.com>
2020-05-10 20:22:09 -0400

    adding changes in my feature branch

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I believe I can fly
+          I was changed in the feature branch
         </p>
         <a
           className="App-link"
